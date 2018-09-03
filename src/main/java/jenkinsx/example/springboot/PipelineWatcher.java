d0699660c8ee68c9f0269172c039dc412e8302ca
Pete Muir <pmuir@bleepbleep.org.uk>
2018-09-03 13:58:14 +0100

    Add pipeline watch

---
+++ src/main/java/jenkinsx/example/springboot/PipelineWatcher.java
@@ -0,0 +1,64 @@
+package jenkinsx.example.springboot;
+
+import freemarker.template.SimpleDate;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import io.fabric8.kubernetes.client.Watcher;
+import io.jenkins.x.client.PipelineClient;
+import io.jenkins.x.client.kube.PipelineActivity;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Component
+public class PipelineWatcher {
+
+    private final SimpleDateFormat timestampFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+    private final Logger log = Logger.getLogger(PipelineWatcher.class.getName());
+
+    private final PipelineClient pipelineClient;
+    private Date creationTimestamp;
+
+    public PipelineWatcher() {
+        this.pipelineClient = PipelineClient.newInstance();
+    }
+
+    private void watch() {
+        Watcher<PipelineActivity> listener = new Watcher<PipelineActivity>() {
+            @Override
+            public void eventReceived(Action action, PipelineActivity pipelineActivity) {
+                try {
+                    Date ct = timestampFormat.parse(pipelineActivity.getMetadata().getCreationTimestamp());
+                    PipelineWatcher.this.creationTimestamp = PipelineWatcher.this.creationTimestamp == null ? ct : creationTimestamp;
+                    if (ct.getTime() > creationTimestamp.getTime()) {
+                        PipelineWatcher.this.creationTimestamp = ct;
+                        log.log(Level.INFO, "New Event at ", timestampFormat.format(ct));
+                        log.log(Level.INFO, pipelineActivity.);
+                    }
+                } catch (ParseException e) {
+                    log.log(Level.SEVERE, "Failed to parse creationTimestamp " + pipelineActivity.getMetadata().getCreationTimestamp(), e);
+                }
+            }
+
+            @Override
+            public void onClose(KubernetesClientException e) {
+                PipelineWatcher.this.log.log(Level.INFO, "Reconnect stream");
+                PipelineWatcher.this.watch();
+            }
+        };
+        this.pipelineClient.addListener(listener);
+    }
+
+    public void start() {
+        this.watch();
+        this.pipelineClient.start();
+    }
+
+}
--- src/main/java/jenkinsx/example/springboot/SpringBootWebApplication.java
+++ src/main/java/jenkinsx/example/springboot/SpringBootWebApplication.java
@@ -1,20 +1,23 @@
 package jenkinsx.example.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.channels.Pipe;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringBootWebApplication extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootWebApplication.class);
-	}
-
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SpringBootWebApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(SpringBootWebApplication.class, args);
+		ctx.getBean(PipelineWatcher.class).start();
 	}
 
 }
